54183dbd297271c29abbafded171be26f6bd8e95
jkarlin929 <osida11572@gmail.com>
2018-12-18 18:42:04 -0500

    add reindeer exercise

---
+++ reindeer.rb
@@ -0,0 +1,22 @@
+# Initialize a Reinder Class
+
+# give your reindeer a name and a location
+# make the location readable
+# make the name readable and writable
+
+# define instance methods for name and location
+# starting location is THE NORTH POLE, duh!
+
+# create a take_off method that take altitude as a parameter.
+# write a statement declaring your reindeer took off.
+# write a statement declaring your reindeer ascended to the altitude.
+
+# create a land method that takes a location as a parameter.
+# write a statement saying your reindeer landed safely in that location.
+
+# Name your reindeer
+# Write a statement indicating the name and location
+# Call the take off method with an altitude of 3000 ft
+# Call the land method with a city in Africa
+# Change your reindeers name
+# Write a statement indicating the new name and location
