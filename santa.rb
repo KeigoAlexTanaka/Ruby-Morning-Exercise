544245a9f6fe1023a5cb241f55c161e1a2d427a7
jkarlin929 <osida11572@gmail.com>
2018-12-18 19:29:15 -0500

    add santa exercise

---
+++ santa.rb
@@ -0,0 +1,58 @@
+# Initialize SANTA CLASS
+
+# set attributes ethnicity, age, gender
+
+
+# define instance methods for gender, ethnicity, age
+
+# write a speak method giving Santa something to say
+
+
+# write a eat_milk_and_cookies method with a cookie_type parameter
+# write statement saying that was a good cookie and print the cookie
+
+
+# write a reindeer_ranking method with all of santas reindeers
+
+
+# write celebrate_birthday method indicating Santa is now one year older
+
+
+#Initialize Santa class starting with a male, polynesian Santa
+
+
+# Print methods for Santa class
+
+
+
+# Print Santa's age
+
+# Print Santa's latest age
+
+# Print method that moves reindeer position, Vixen is now last, Vixen knows what he did
+
+# Print Santa's ethnicity
+
+# Re-assign gender attribute
+
+
+# Add more Santas to the class
+# create a santas array
+
+# declare nine example_genders
+
+# declare nine example_ethnicities
+
+# create nine new santas to add to our santas array:
+# loop through the example_genders array and for each gender, push the new santa with one of the ethnicities from example_ethnicities
+# into our santas array
+
+
+
+
+# Set santas value to zero
+
+# Loop through the Santas 10 times to provide random genders, ethnicities and ages for Santas
+# For clarity, inside the loop, add this line: puts "New Santa data:"
+# print a random gender and ethnicity for each iteration, this may require a look at the ruby docs for arrays
+# Santas age should range from 0 to 140
